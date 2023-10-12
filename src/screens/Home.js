ad141b5aeff737b24bf889cc532b71584549d54a
Nunes-Will <nuneswillroberto@gmail.com>
2023-10-12 20:49:36 -0300

    Imagem pegando.

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,22 +1,29 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, ScrollView,Image } from 'react-native';
-import { Button, Card } from 'react-native-paper';
+import React from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Image,
+} from "react-native";
+import { Button, Card } from "react-native-paper";
 
-import Engrenagem from '../../icons/engrenagem.png'
-import Login from '../../icons/login.png'
-
-
+import Engrenagem from "../../icons/engrenagem.png";
+import Login from "../../icons/login.png";
 
 const HomeScreen = ({ navigation }) => {
   const people = [
     {
       id: 1,
-      name: 'Maria Silva',
-      jobType: 'Limpeza',
-      location: 'São Paulo',
+      name: "Maria Silva",
+      jobType: "Limpeza",
+      location: "São Paulo",
       age: 30,
-      price: 'R$ 50/hora',
-      image: URL = "google.com",
+      price: "R$ 50/hora",
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id:2,
@@ -25,6 +32,8 @@ const HomeScreen = ({ navigation }) => {
       location: 'Rio de Janeiro',
       age: 28,
       price: 'R$ 40/hora',
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id:3,
@@ -33,6 +42,8 @@ const HomeScreen = ({ navigation }) => {
       location: 'São Paulo',
       age: 70,
       price: 'R$ 100/mês',
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id:4,
@@ -40,7 +51,8 @@ const HomeScreen = ({ navigation }) => {
       jobType: 'Encanador',
       location: 'São Paulo',
       age: 30,
-      price: 'R$ 35/hora',
+      price: 'R$ 35/hora',image:
+      "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id:5,
@@ -49,6 +61,8 @@ const HomeScreen = ({ navigation }) => {
       location: 'São Paulo',
       age: 72,
       price: 'R$ 70/hora',
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id: 6,
@@ -57,6 +71,8 @@ const HomeScreen = ({ navigation }) => {
       location: 'São Paulo',
       age: 48,
       price: 'R$ 67/hora',
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id:7,
@@ -65,6 +81,8 @@ const HomeScreen = ({ navigation }) => {
       location: 'São Paulo',
       age: 33,
       price: 'R$ 50/hora',
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       ide:8,
@@ -72,7 +90,8 @@ const HomeScreen = ({ navigation }) => {
       jobType: 'Tira pó',
       location: 'São Paulo',
       age: 19,
-      price: 'R$ 70/hora',
+      price: 'R$ 70/hora',image:
+      "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
     {
       id:9,
@@ -81,14 +100,8 @@ const HomeScreen = ({ navigation }) => {
       location: 'São Paulo',
       age: 39,
       price: 'R$ 114/hora',
-    },
-    {
-      id:10,
-      name: 'Thiago Silva',
-      jobType: 'Eletrecista',
-      location: 'São Paulo',
-      age: 28,
-      price: 'R$ 67/hora',
+      image:
+        "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
     },
   ];
 
@@ -103,22 +116,32 @@ const HomeScreen = ({ navigation }) => {
           placeholder="Pesquisar..."
           placeholderTextColor="#999"
         />
-        <TouchableOpacity onPress={() => navigation.navigate("Cadastro")} style={styles.button}>
+        <TouchableOpacity
+          onPress={() => navigation.navigate("Cadastro")}
+          style={styles.button}
+        >
           <Image source={Login} style={styles.image}></Image>
         </TouchableOpacity>
       </View>
-      
+
       {people.map((person, index) => (
         <TouchableOpacity
           key={index}
           style={styles.card}
           onPress={() => navigation.navigate("Detail")}
         >
-          <Text style={styles.cardText}>{`Nome: ${person.name}`}</Text>
-          <Text style={styles.cardText}>{`Trabalho Doméstico: ${person.jobType}`}</Text>
-          <Text style={styles.cardText}>{`Localidade: ${person.location}`}</Text>
-          <Text style={styles.cardText}>{`Idade: ${person.age} anos`}</Text>
-          <Text style={styles.cardText}>{`Preço: ${person.price}`}</Text>
+          <Image source={{ uri: person.image }} style={styles.cardImage} />
+          <View style={styles.cardTextContainer}>
+            <Text style={styles.cardText}>{`Nome: ${person.name}`}</Text>
+            <Text
+              style={styles.cardText}
+            >{`Trabalho Doméstico: ${person.jobType}`}</Text>
+            <Text
+              style={styles.cardText}
+            >{`Localidade: ${person.location}`}</Text>
+            <Text style={styles.cardText}>{`Idade: ${person.age} anos`}</Text>
+            <Text style={styles.cardText}>{`Preço: ${person.price}`}</Text>
+          </View>
         </TouchableOpacity>
       ))}
     </ScrollView>
@@ -128,14 +151,14 @@ const HomeScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#3B8AEB',
+    backgroundColor: "#3B8AEB",
     paddingHorizontal: 16,
     paddingTop: 50,
   },
   header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
     marginBottom: 20,
   },
   button: {
@@ -143,24 +166,34 @@ const styles = StyleSheet.create({
   },
   searchBar: {
     flex: 1,
-    height: 40,
-    backgroundColor: '#eee',
+    height: 35,
+    backgroundColor: "#eee",
     borderRadius: 20,
     paddingHorizontal: 16,
     marginHorizontal: 10,
   },
   card: {
-    backgroundColor: '#fff',
+    backgroundColor: "#fff",
     borderRadius: 10,
     padding: 10,
     marginBottom: 10,
+    flexDirection: "row",
+  },
+  cardImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 10,
+  },
+  cardTextContainer: {
+    marginLeft: 10,
+    flex: 1,
   },
   cardText: {
     fontSize: 13,
   },
   image: {
-    width: 25,
-    height: 25,
+    width: 30,
+    height: 30,
   },
 });
 
