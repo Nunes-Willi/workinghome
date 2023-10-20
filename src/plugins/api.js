5966194f60fd6342ca3cfa6a6b91da4c01b77f1f
BCD15 <bruno69correadias@gmail.com>
2023-10-20 09:44:36 -0300

    erro ao puxar informações da api

---
+++ src/plugins/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const { MY_IP } = process.env;
+
+const api = axios.create({
+  baseURL: `http://${MY_IP}:19003/`
+});
+
+export default api;
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   View,
   Text,
@@ -9,83 +9,95 @@ import {
   Image,
 } from "react-native";
 
-import dnaGertrudes from "../../assets/dnaGertrudes.png";
+import api from '../plugins/api';
+
 import Engrenagem from "../../icons/engrenagem.png";
 import Login from "../../icons/login.png";
 import MapSreen from "../../components/App";
 
 export default function HomeScreen ({ navigation }) {
+  const [people, setPeople] = React.useState([]);
+
+  const getPeople = async () => {
+    const {data} = await api.get('/people/')
+    setPeople(data)
+  }
+
+  useEffect(() => {
+    getPeople()
+  }, [])
+
   // const people = [
   //   {
-  //     id: 1,
-  //     name: "Dna. Gertrudes",
-  //     jobType: "Limpeza",
-  //     location: "Av. Gertulio Vargas 369 - Joinville SC",
-  //     age: 72,
-  //     price: "R$ 110/Diária",
-  //     image:
-  //       "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "João Oliveira",
-  //     jobType: "Cozinhar",
-  //     location: "Rio de Janeiro",
-  //     age: 28,
-  //     price: "R$ 40/hora",
-  //     image:
-  //       "https://img.freepik.com/fotos-gratis/chef-profissional-cozinheiro-em-uniforme-branco-e-chapeu-de-cozinheiro-apontando-para-o-lado-com-o-dedo-indicador-parecendo-confiante-em-pe-sobre-um-fundo-branco_141793-20622.jpg?",
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Therezinha Gertrudes Pires",
-  //     jobType: "Babá",
-  //     location: "São Paulo",
-  //     age: 70,
-  //     price: "R$ 100/mês",
-  //     image:
-  //       "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "Mario Bros",
-  //     jobType: "Encanador",
-  //     location: "São Paulo",
-  //     age: 30,
-  //     price: "R$ 35/hora",
-  //     image:
-  //       "https://maridodealuguel.com.br/wp-content/uploads/2021/12/352-3524981_plumbing-plumbing-man.png",
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Paula Vieira",
-  //     jobType: "Cozinhar",
-  //     location: "São Paulo",
-  //     age: 34,
-  //     price: "R$ 70/hora",
-  //     image:
-  //       "https://blog.unifil.br/wp-content/uploads/shutterstock_1889627056.jpg",
-  //   },
-  //   {
-  //     id: 6,
-  //     name: "Luigi Bros",
-  //     jobType: "Controle de Pragas",
-  //     location: "São Paulo",
-  //     age: 48,
-  //     price: "R$ 67/hora",
-  //     image:
-  //       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc5E_v2LaL7I1RirG84CsXEpL2qdUpvPxCKg&usqp=CAU",
-  //   },
-  //   {
-  //     id: 7,
-  //     name: "Pedro Silva",
-  //     jobType: "Limpeza de calha",
-  //     location: "São Paulo",
-  //     age: 33,
-  //     price: "R$ 50/hora",
-  //     image:
-  //       "https://img.freepik.com/fotos-premium/retrato-de-uma-velhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvwa8vu5oInEGAynvtgJTK-t9_z8nTC9jAww&usqp=CAUha-segurando-dinheiro-isolado_246836-889.jpg",
-  //   },
+    //   id: 1,
+    //   name: "Dna. Gertrudes",
+    //   jobType: "Limpeza",
+    //   location: "Av. Gertulio Vargas 369 - Joinville SC",
+    //   age: 72,
+    //   price: "R$ 110/Diária",
+    //   image:
+    //     "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
+    // },
+    // {
+    //   id: 2,
+    //   name: "João Oliveira",
+    //   jobType: "Cozinhar",
+    //   location: "Rio de Janeiro",
+    //   age: 28,
+    //   price: "R$ 40/hora",
+    //   image:
+    //     "https://img.freepik.com/fotos-gratis/chef-profissional-cozinheiro-em-uniforme-branco-e-chapeu-de-cozinheiro-apontando-para-o-lado-com-o-dedo-indicador-parecendo-confiante-em-pe-sobre-um-fundo-branco_141793-20622.jpg?",
+    // },
+    // {
+    //   id: 3,
+    //   name: "Therezinha Gertrudes Pires",
+    //   jobType: "Babá",
+    //   location: "São Paulo",
+    //   age: 70,
+    //   price: "R$ 100/mês",
+    //   image:
+    //     "https://img.freepik.com/fotos-premium/retrato-de-uma-velha-segurando-dinheiro-isolado_246836-889.jpg",
+    // },
+    // {
+    //   id: 4,
+    //   name: "Mario Bros",
+    //   jobType: "Encanador",
+    //   location: "São Paulo",
+    //   age: 30,
+    //   price: "R$ 35/hora",
+    //   image:
+    //     "https://maridodealuguel.com.br/wp-content/uploads/2021/12/352-3524981_plumbing-plumbing-man.png",
+    // },
+    // {
+    //   id: 5,
+    //   name: "Paula Vieira",
+    //   jobType: "Cozinhar",
+    //   location: "São Paulo",
+    //   age: 34,
+    //   price: "R$ 70/hora",
+    //   image:
+    //     "https://blog.unifil.br/wp-content/uploads/shutterstock_1889627056.jpg",
+    // },
+    // {
+    //   id: 6,
+    //   name: "Luigi Bros",
+    //   jobType: "Controle de Pragas",
+    //   location: "São Paulo",
+    //   age: 48,
+    //   price: "R$ 67/hora",
+    //   image:
+    //     "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTc5E_v2LaL7I1RirG84CsXEpL2qdUpvPxCKg&usqp=CAU",
+    // },
+    // {
+    //   id: 7,
+    //   name: "Pedro Silva",
+    //   jobType: "Limpeza de calha",
+    //   location: "São Paulo",
+    //   age: 33,
+    //   price: "R$ 50/hora",
+    //   image:
+    //     "https://img.freepik.com/fotos-premium/retrato-de-uma-velhttps://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvwa8vu5oInEGAynvtgJTK-t9_z8nTC9jAww&usqp=CAUha-segurando-dinheiro-isolado_246836-889.jpg",
+    // },
   // ];
 
   return (
@@ -108,26 +120,26 @@ export default function HomeScreen ({ navigation }) {
         </View>
       <MapSreen />
       <ScrollView style={styles.container}>
-        {people.map((person, index) => (
+        {people.map((people) => (
           <TouchableOpacity
-            key={index}
+            key={people.id}
             style={styles.card}
             onPress={() => navigation.navigate("Detail")}
           >
             <Image
-              source={person.id === 1 ? dnaGertrudes : { uri: person.image }}
+              source={{ uri: people.foto }}
               style={styles.cardImage}
             />
             <View style={styles.cardTextContainer}>
-              <Text style={styles.cardText}>{`Nome: ${person.name}`}</Text>
+              <Text style={styles.cardText}>{`Nome: ${people.name}`}</Text>
               <Text
                 style={styles.cardText}
-              >{`Trabalho Doméstico: ${person.jobType}`}</Text>
+              >{`Trabalho Doméstico: ${people.servico}`}</Text>
               <Text
                 style={styles.cardText}
-              >{`Localidade: ${person.location}`}</Text>
-              {/* <Text style={styles.cardText}>{`Idade: ${person.age} anos`}</Text> */}
-              <Text style={styles.cardText}>{`Preço: ${person.price}`}</Text>
+              >{`Localidade: ${people.local}`}</Text>
+              <Text style={styles.cardText}>{`Idade: ${people.idade} anos`}</Text>
+              <Text style={styles.cardText}>{`Preço: ${people.valor}`}</Text>
               <Text
                 style={{ color: "#837F7F", fontWeight: "bold", marginTop: 20 }}
               >
@@ -165,7 +177,7 @@ const styles = StyleSheet.create({
   },
   buttonLogin: {
     padding: 10,
-    backgroundColor: 'trasparent',
+    backgroundColor: 'transparent',
     borderRadius: 40,
   },
   searchBar: {
